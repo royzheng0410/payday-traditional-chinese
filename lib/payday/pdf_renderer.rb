0ba2ef48d6f98e3da2afa669027fdceb260e590d
Roy <royzheng0410@gmail.comm>
2016-02-11 15:30:32 +1030

    tweak font creation

--- lib/payday/pdf_renderer.rb
+++ lib/payday/pdf_renderer.rb
@@ -21,8 +21,8 @@ module Payday
       p "========================"
       p Rails.root
       pdf.font_size(8)
-      pdf.font_families.update("123" => {:normal => "#{Rails.root}/app/assets/fonts/wt006.ttf"})
-      pdf.font("123")
+      font_families.update("123" => {:normal => "#{Rails.root}/app/assets/fonts/wt006.ttf"})
+      pdf.font = font "123"
       stamp(invoice, pdf)
       company_banner(invoice, pdf)
       bill_to_ship_to(invoice, pdf)
